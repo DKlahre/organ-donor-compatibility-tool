5fda01c45be0234e2f20d3c9914e959728be117a
Daniel Claire <oregadan@yahoo.com>
2024-06-16 20:37:32 -0500

    first SQL statements

--- src/main/java/com/proj/organtransplantapp/Models/DatabaseDriver.java
+++ src/main/java/com/proj/organtransplantapp/Models/DatabaseDriver.java
@@ -3,9 +3,7 @@ package com.proj.organtransplantapp.Models;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseDriver  {
 
@@ -20,4 +18,76 @@ public class DatabaseDriver  {
             e.printStackTrace();
         }
     }
+
+    public ResultSet searchHeartList(int ageLower, int ageHigher) {
+
+        Statement statement;
+        ResultSet resultSet = null;
+        try {
+            statement = this.conn2.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM Heart WHERE Age BETWEEN '" + ageLower + "' AND '" + ageHigher + "';");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    public ResultSet searchKidneyList(int ageLower, int ageHigher) {
+
+        Statement statement;
+        ResultSet resultSet = null;
+        try {
+            statement = this.conn2.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM Kidney WHERE Age BETWEEN '" + ageLower + "' AND '" + ageHigher + "';");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    public ResultSet searchLiverList(int ageLower, int ageHigher) {
+
+        Statement statement;
+        ResultSet resultSet = null;
+        try {
+            statement = this.conn2.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM Liver WHERE Age BETWEEN '" + ageLower + "' AND '" + ageHigher + "';");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    public ResultSet searchLungList(int ageLower, int ageHigher) {
+
+        Statement statement;
+        ResultSet resultSet = null;
+        try {
+            statement = this.conn2.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM Lung WHERE Age BETWEEN '" + ageLower + "' AND '" + ageHigher + "';");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    public ResultSet searchPancreasList(int ageLower, int ageHigher) {
+
+        Statement statement;
+        ResultSet resultSet = null;
+        try {
+            statement = this.conn2.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM Pancreas WHERE Age BETWEEN '" + ageLower + "' AND '" + ageHigher + "';");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+
+
+
+
+
+
 }
