ae4de5672832c8df34f5b9bd20de655d16a998fb
Daniel Claire <oregadan@yahoo.com>
2024-07-03 13:59:18 -0500

    resultSet values into TempTable

--- src/main/java/com/proj/organtransplantapp/Models/DatabaseDriver.java
+++ src/main/java/com/proj/organtransplantapp/Models/DatabaseDriver.java
@@ -113,65 +113,59 @@ public class DatabaseDriver  {
         return resultSet;
     }
 
-    public ResultSet searchDonorsDB(String organ, String bloodType ) throws SQLException {
+    public ResultSet searchDonorsDB(String organParam, String bloodType ) throws SQLException {
         Statement statement2;
         Statement statement3;
+        Statement statement10;
         ResultSet resultSet = null;
-        String fName = "hawr";
-        String lName = "agaz";
-        String gender = "trshgr";
-        String age = "sthrg";
-        String weight = "trhegr";
-        String email = "agh";
-        String orgn = "yjrth";
-        String bloodtype = "thgr";
-        String dp1 = "hetr";
-        String dp2 = "trhrg";
-        String dp3 = "sthrg";
-        String abc1 = "ynthgr";
-        String abc2 = "mhynth";
-        String abc3 = "mnngr";
-        String drb1 = "yrnthebgr";
-        String drp2 = "mtnghg";
-        String drb3 = "yrn";
-        String dq1 = "ngr";
-        String dq2 = "nhg";
-        String dq3 = "ynthegr";
-        String mica1 = "nrth";
-        String mica2 = "hngb";
-        String mica3 = "mhng";
 
         try {
             statement2 = this.conn2.createStatement();
-            resultSet = statement2.executeQuery("SELECT * FROM '" + organ + "' Where BloodType = '" + bloodType + "';");
+            resultSet = statement2.executeQuery("SELECT * FROM '" + organParam + "' Where BloodType = '" + bloodType + "';");
         } catch (SQLException e) {
             e.printStackTrace();
         }
         while (resultSet.next()) {
             try {
+                String fName = resultSet.getString(1);
+                String lName = resultSet.getString(2);
+                String gender = resultSet.getString(3);
+                String age = resultSet.getString(4);
+                String weight = resultSet.getString(5);
+                String email = resultSet.getString(6);
+                String organ = resultSet.getString(7);
+                String bloodtype = resultSet.getString(8);
+                String dp1 = resultSet.getString(9);
+                String dp2 = resultSet.getString(10);
+                String dp3 = resultSet.getString(11);
+                String abc1 = resultSet.getString(12);
+                String abc2 = resultSet.getString(13);
+                String abc3 = resultSet.getString(14);
+                String drb1 = resultSet.getString(15);
+                String drp2 = resultSet.getString(16);
+                String drb3 = resultSet.getString(17);
+                String dq1 = resultSet.getString(18);
+                String dq2 = resultSet.getString(19);
+                String dq3 = resultSet.getString(20);
+                String mica1 = resultSet.getString(21);
+                String mica2 = resultSet.getString(22);
+                String mica3 = resultSet.getString(23);
                 statement3 = this.conn2.createStatement();
-                statement3.executeUpdate("INSERT INTO TempTable(FirstName, LastName, Gender, Age, Weight, Email, Organ, BloodType, DP1, DP2, DP3, ABC1, ABC2, ABC3, DRB1, DRB2, DRB3, DQ1, DQ2, DQ3, MICA1, MICA2, MICA3)" +
-                        "VALUES ('"+fName +"', '"+ lName+"', '"+ gender+"', '"+ age+"', '"+ weight+"', '"+ email+"', '"+ orgn+"', '"+ bloodtype+"', '"+ dp1+"', '"+ dp2+"', '"+ dp3+"', '"+ abc1+"', '"+ abc2+"', '"+ abc3+"', '"+ drb1+"', '"+ drp2+"', '"+ drb3+"','" +dq1+"', '"+ dq2+"', '"+ dq3+"', '"+mica1+"', '"+ mica2+"', '"+ mica3+"');");
+                statement3.executeUpdate("INSERT INTO TempTable(FirstName, LastName, Gender, Age, Weight, Email, Organ, BloodType, " +
+                        "DP1, DP2, DP3, ABC1, ABC2, ABC3, DRB1, DRB2, DRB3, DQ1, DQ2, DQ3, MICA1, MICA2, MICA3)" +
+                        "VALUES ('"+fName +"', '"+ lName+"', '"+ gender+"', '"+ age+"', '"+ weight+"', '"+ email+"', '"+ organ+"', '"+ bloodtype+
+                        "', '"+ dp1+"', '"+ dp2+"', '"+ dp3+"', '"+ abc1+"', '"+ abc2+"', '"+ abc3+"', '"+ drb1+"', '"+ drp2+"', '"+ drb3+
+                        "','" +dq1+"', '"+ dq2+"', '"+ dq3+"', '"+mica1+"', '"+ mica2+"', '"+ mica3+"');");
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
 
+//        statement10 = this.conn2.createStatement();
+//        int deletedRows = statement10.executeUpdate("DELETE FROM TempTable;");
+
         return resultSet;
     }
-
-//    public ResultSet getAllCandidatesData() {
-//        Statement statement;
-//        ResultSet resultSet = null;
-//        try {
-//            statement = this.conn1.createStatement();
-//            resultSet = statement.executeQuery("SELECT * FROM Candidates;");
-//        } catch (SQLException e) {
-//            System.out.println("We got a problem here - getAllCandidatesData()");
-//            e.printStackTrace();
-//        }
-//        return resultSet;
-//    }
 
 
 
